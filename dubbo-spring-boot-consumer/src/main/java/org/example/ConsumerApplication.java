1fa88f28d3ffdddcc14f8a951633e7c41bcf19ff
rockleeprc <rockleeprc@outlook.com>
2019-12-24 14:47:10 +0800

    edit

--- dubbo-spring-boot-consumer/src/main/java/org/example/ConsumerApplication.java
+++ dubbo-spring-boot-consumer/src/main/java/org/example/ConsumerApplication.java
@@ -3,9 +3,11 @@ package org.example;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDubbo
+@EnableHystrix
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class,args);
--- dubbo-spring-boot-consumer/src/main/java/org/example/service/ConsumerService.java
+++ dubbo-spring-boot-consumer/src/main/java/org/example/service/ConsumerService.java
@@ -2,9 +2,11 @@ package org.example.service;
 
 import com.example.domain.Person;
 import com.example.service.IPersonServer;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -14,12 +16,20 @@ public class ConsumerService {
      * check = false 只有全局、类级别，没有方法级别
      * retries = 2 默认2，不包含第一次请求失败，重试2次，总共3次请求
      */
-    @Reference(version = "1.0.0",check = true)
+    @Reference(version = "1.0.0", check = true)
     private IPersonServer personServer;
 
-
+    @HystrixCommand(fallbackMethod = "reliable")
     public List<Person> findAll() {
+        long time = System.currentTimeMillis();
+        if (time % 2 == 0) {
+            int result = 1 / 0;
+        }
         return personServer.findAll();
+    }
 
+
+    public List<Person> reliable() {
+        return Arrays.asList(new Person("hystrix"));
     }
 }
